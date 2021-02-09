1bcbcfbad8a57ac4435e58fb7c98c1a30f9e3665
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2021-02-09 02:16:35 +0200

    x

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,19 +1,37 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+import * as express from 'express';
 // Require Express to run server and routes
+const express =require ('express')
+const bodyParser =require('body-parser')
+const cors = require('cors')
+const app = express()
 
+app.use(cors())
 // Start up an instance of app
-
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
 // Setup Server
+const port = 5050
+const server =app.listen(port ,()=>{
+    console.log(`server is running on localhost : ${port}`)
+})
+app.get('/all' ,(req,res)=>{
+    res.send(projectData).status(200)
+
+})
+app.post('/add',(req,res)=>{
+    console.log(req.body)
+    projectData ={
+        date : req.body.date,
+        temp :req.body.temp,
+        content : req.body.content
+    }
+        res.send(projectData).status(200)
+
+})
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,29 @@
 /* Global Variables */
 
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let URL ='http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='
+const KEY ='c5eb172d4c9aabe88bf6a2fa5dc7a42d'
+let zip = document.getElementById('zip').value
+const feelings = document.getElementById('feelings').value
+
+document.getElementById('generate').addEventListener('click',(e)=>{
+    getweatherData(URL,zip ,KEY).then((data)=>{
+        console.log(data)
+    })
+})
+
+const getweatherData =async function(URL,zip ,KEY){
+    const res = await fetch(URL+zip+KEY)
+    try{
+        const data = await res.json()
+        return data
+        
+
+    }catch(error){
+        console.log(error)
+    }
+}
